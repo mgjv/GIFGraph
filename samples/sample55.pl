e763eeae39c3b22e26cd096861e79bda98174574
mgjv <mgjv@18c24718-09bb-4ca4-91e6-790a849c9d05>
1999-12-29 12:29:39 +0000

    Use PI, instead of 3.1415..
    git-svn-id: file:///tmp/svnGG/trunk@38 18c24718-09bb-4ca4-91e6-790a849c9d05

--- samples/sample55.pl
+++ samples/sample55.pl
@@ -1,12 +1,11 @@
-use GIFgraph::lines;
 use strict;
+use GIFgraph::lines;
+
+use constant PI => 4 * atan2(1,1);
 
 print STDERR "Processing sample 5-5\n";
 
-# The reverse is in here, because I thought the falling line was
-# depressing, but I was too lazy to retype the data set
-
-my @x = map {$_ * 3 * 3.14159/100} (0 .. 100);
+my @x = map {$_ * 3 * PI/100} (0 .. 100);
 my @y = map sin, @x;
 my @z = map cos, @x;
 
@@ -15,14 +14,14 @@ my @data = (\@x,\@y,\@z);
 my $my_graph = new GIFgraph::lines();
 
 $my_graph->set(
-	x_label 			=> 'Angle (Radians)',
-	y_label 			=> 'Trig Function Value',
-	x_tick_number 		=> 'auto',
-	y_tick_number 		=> 'auto',
-	title 				=> 'Sine and Cosine',
-	line_width 			=> 1,
-	x_label_position 	=> 1/2,
-	r_margin 			=> 15,
+	x_label				=> 'Angle (Radians)',
+	y_label				=> 'Trig Function Value',
+	x_tick_number		=> 'auto',
+	y_tick_number		=> 'auto',
+	title				=> 'Sine and Cosine',
+	line_width			=> 1,
+	x_label_position	=> 1/2,
+	r_margin			=> 15,
 );
 
 $my_graph->set_legend('Thanks to Scott Prahl');
