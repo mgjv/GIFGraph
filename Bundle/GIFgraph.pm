db169420512b7ddc4c735e13c95c7a4838f648d7
mgjv <mgjv@18c24718-09bb-4ca4-91e6-790a849c9d05>
2000-01-04 05:55:34 +0000

    Added Bundle
    git-svn-id: file:///tmp/svnGG/trunk@44 18c24718-09bb-4ca4-91e6-790a849c9d05

---
+++ Bundle/GIFgraph.pm
@@ -0,0 +1,32 @@
+package Bundle::GIFgraph;
+
+$VERSION = '1.20';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Tk - CPAN Bundle for GIFgraph and its dependencies
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::GIFgraph'>
+
+=head1 CONTENTS
+
+GD::Text			- Will pull in all the GDTextUtils
+
+GD::Graph
+
+GIFgraph
+
+=head1 DESCRIPTION
+
+Install The necessities for versions of GIFgraph above 1.20.
+
+=head1 AUTHOR
+
+Martien Verbruggen (mgjv@comdyn.com.au)
+
